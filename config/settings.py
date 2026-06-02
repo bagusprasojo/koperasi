f5bbdcafd3ef656d3743926109bc930fa1d73ddd
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2026-06-02 13:39:40 +0700

    update akses media khusus internal

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# admin url
+ADMIN_URL = os.getenv("ADMIN_URL", "admin/")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
     path('', include('core.urls')),
     path('inventory/', include('inventory.urls')),
     path('members/', include('members.urls')),
